2007081fba717f47e9a11e159731f3d4f2d51283
David Ji <jz_1106@163.com>
2020-07-07 10:52:34 +0800

    Consul

---
+++ consul-consumer/src/test/java/net/tongark/springcloud/consulconsumer/ConsulConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tongark.springcloud.consulconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- consul-producer/src/main/java/net/tongark/springcloud/consulproducer/ConsulProducerApplication.java
+++ consul-producer/src/main/java/net/tongark/springcloud/consulproducer/ConsulProducerApplication.java
@@ -2,8 +2,10 @@ package net.tongark.springcloud.consulproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient //表示支持服务发现
 public class ConsulProducerApplication {
 
     public static void main(String[] args) {
