751a73d657637b66d4239f36bfa334af66c27516
David Ji <jz_1106@163.com>
2020-08-12 16:50:17 +0800

    R7 : Bus

---
+++ eureka-bus-amqp/src/main/java/net/tongark/springcloud/eurekabusamqp/EurekaBusAmqpApplication.java
@@ -0,0 +1,13 @@
+package net.tongark.springcloud.eurekabusamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaBusAmqpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaBusAmqpApplication.class, args);
+    }
+
+}
---
+++ eureka-bus-amqp/src/main/java/net/tongark/springcloud/eurekabusamqp/controller/TestConfigController.java
@@ -0,0 +1,30 @@
+package net.tongark.springcloud.eurekabusamqp.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class TestConfigController {
+
+    @Value("${from}")
+    private String from;
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/from")
+    public String from(){
+        return this.from;
+    }
+
+    @RequestMapping("/env_from")
+    public String envFrom(){
+        return env.getProperty("from")+" ENV";
+    }
+}
